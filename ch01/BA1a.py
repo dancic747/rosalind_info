4eb8fe38682de936fab45ac3cb12186e47219670
dancic747 <doris.ancic@gmail.com>
2022-03-04 01:10:25 +0100

    BA1A verified

--- ch01/BA1a.py
+++ ch01/BA1a.py
@@ -1,18 +1,17 @@
 #BA1A
 #Compute the Number of Times a Pattern Appears in a Text
 
-
 def Count(text, pattern):
     count = 0
-    for i in range(0, len(text)-len(pattern)+1):
+    for i in range(len(text)-len(pattern)+1):
         if (text[i:i+len(pattern)] == pattern):
             count+=1
     return count
 
 
 if __name__ == '__main__':
+    with open("C:/Users/Doris/Downloads/rosalind_ba1a.txt", "r") as f:
+        text = f.readline().strip()
+        pattern = f.readline().strip()
+    print(Count(text, pattern))
     
-    text=input('text: ')
-    pattern=input('pattern:  ')
-    c=Count(text,pattern)
-    print(c)
