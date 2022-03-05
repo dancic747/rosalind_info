ac93ddf2b31c091500add9219be4cc3ed80bfafb
dancic747 <doris.ancic@gmail.com>
2022-03-05 21:09:24 +0100

    BA1F fixed & verified

--- ch01/BA1f.py
+++ ch01/BA1f.py
@@ -1,29 +1,26 @@
 #BA1F
 #Find a Position in a Genome Minimizing the Skew
 
-def compute_skew(text):
-    countC=0
-    countG=0
-    for i in range(len(text)):
-        if text[i]=='C': countC+=1
-        if text[i]=='G': countG+=1
-    skew=countG-countC
-    return skew
-
 
 def find_min_skew_positions(text):
-    skew=[]
-    min_skew_positions=[]
+    skew=0
+    all_skews=[]
+    all_skews.append(skew)
     for i in range(len(text)):
-        skew.append(compute_skew(text[:i:1]))
-    min_skew=min(skew)
-    for i in range(len(skew)):
-        if (skew[i]==min_skew): min_skew_positions.append(i)
+        if(text[i]=='C'): skew-=1
+        elif (text[i]=='G'): skew+=1
+        all_skews.append(skew)
+    min_skew=min(all_skews)
+    min_skew_positions=[]
+    for j in range(len(all_skews)):
+        if(all_skews[j]==min_skew): min_skew_positions.append(j)
     return min_skew_positions
-
+    
 
 if __name__=='__main__':
     with open('C:/Users/Doris/Downloads/rosalind_ba1f.txt','r') as f:
         text=f.readline().strip()
     min_skew_positions=find_min_skew_positions(text)
     print(*min_skew_positions,' ')
+
+    
