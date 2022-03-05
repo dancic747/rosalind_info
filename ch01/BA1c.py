02b0735a85fdaad9d37bf981caef2a0ef416b455
dancic747 <doris.ancic@gmail.com>
2022-03-05 16:58:53 +0100

    Rosalind: BA1c

---
+++ ch01/BA1c.py
@@ -0,0 +1,18 @@
+#BA1C
+#Find the Reverse Complement of a String
+
+def complement(text):
+    c=''
+    for i in range(len(text)):
+        if (text[i]=='A'): c+='T'
+        elif (text[i]=='T'): c+='A'
+        elif (text[i]=='C'): c+='G'
+        else: c+='C'
+    c=c[::-1]
+    return c
+
+
+if __name__=='__main__':
+    with open("C:/Users/Doris/Downloads/rosalind_ba1c.txt", "r") as f:
+       text = f.readline().strip()
+    print(complement(text))
